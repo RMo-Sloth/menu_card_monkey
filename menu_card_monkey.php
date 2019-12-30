481972acbf85134bccf3bf46183457099d177aec
Rob <robmonhemius@gmail.com>
2019-12-30 20:30:50 +0100

    initial setup

---
+++ menu_card_monkey.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Plugin Name:        Menu Card Monkey
+* Description:        Menu Card Monkey helps you create a restaurant menu for your website.
+* Version:            0.0.0
+* Author:             Rob Monhemius
+* Author URI:         https://www.waardwebsites.nl
+* Requires at least:  5.0.0
+* Requires PHP:       7.0
+* License:            No License
+* License URI:
+* Text Domain:        menu-card-monkey
+* Domain Path:        languages
+**/
